bab2adffd409ec665637e4b69211957762330596
kato-f <kato-f@etic.co.jp>
2023-09-05 16:19:49 +0900

    add: format on save

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -316,12 +316,23 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     event = "BufEnter",
     config = function()
+      local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+      local event = "BufWritePre" -- or "BufWritePost"
+      local async = event == "BufWritePost"
+
       require("null-ls").setup({
         on_attach = function(client, bufnr)
           if client.supports_method("textDocument/formatting") then
-            vim.keymap.set("n", "<S-f>", function()
-              vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-            end, { buffer = bufnr, desc = "[lsp] format" })
+            -- format on save
+            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+            vim.api.nvim_create_autocmd(event, {
+              buffer = bufnr,
+              group = group,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr, async = async })
+              end,
+              desc = "[lsp] format on save",
+            })
           end
         end,
       })
@@ -332,7 +343,7 @@ return {
 
   {
     "MunifTanjim/prettier.nvim",
-    event = "BufWritePre",
+    event = "BufEnter",
     config = function()
       require("prettier").setup({
         bin = "prettierd", -- npm install -g @fsouza/prettierd
