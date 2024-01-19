723ab82a347b122c6c4d5c5274a00ab922287d78
smdhnz <strikerfmy1208@gmail.com>
2024-01-19 18:12:53 +0900

    biome導入

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -212,7 +212,7 @@ return {
 
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.formatting.prettierd,
+					null_ls.builtins.formatting.biome,
 					null_ls.builtins.formatting.stylua,
 				},
 				on_attach = function(client, bufnr)
